9893d46e8f0fdf43254a492fac348777517e7872
Gabry17 <gbianchi528@gmail.com>
2022-04-27 15:43:19 +0200

    aggiunto class d-none dopo 30s

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 //generare 5 numeri random e inserirli in un array
-const div = document.getElementById('number');
 let numRnd = 0;
 let arrayRnd = [];
 
 for(let i = 0; i < 5; i++){
     numRnd = getRndInteger(1,100);
     arrayRnd.push(numRnd);
-    //visualizzare numeri random nel browser
-    div.innerHTML = `Memorizzare i seguenti numeri: ${arrayRnd} `;
 }
+
+//visualizzare numeri random nel browser
+const div = document.getElementById('number');
+div.innerHTML = `Memorizzare i seguenti numeri: ${arrayRnd} `;
+
+//aggiungere classe d-block per 30s
+setTimeout(function(){
+    div.classList.add('d-none');
+},30 * 1000);
+
+
 
 
 
